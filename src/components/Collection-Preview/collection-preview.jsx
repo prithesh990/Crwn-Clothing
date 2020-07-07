bc9ebf18c6bd069ab8f215601480576b9db3aa10
prithesh990 <prithesh.chowta1997@gmail.com>
2020-07-07 18:46:26 +0530

    shop page added

---
+++ src/components/Collection-Preview/collection-preview.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./collection-preview.styles.scss";
+import CollectionItem from "../Collection-Item/collection-item.components";
+
+export const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map(({ id, ...otherCollectionProps }) => (
+            <CollectionItem key={id} {...otherCollectionProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
