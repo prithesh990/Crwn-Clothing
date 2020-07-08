f5dbe62ff5a9c792ec3acaa224da652f5ec923b0
prithesh990 <prithesh.chowta1997@gmail.com>
2020-07-08 21:15:09 +0530

    signup functionality added

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,33 @@ const config = {
     measurementId: "G-CQX0VP7Q6Y"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    //snapshot is used to read the doc  or collections it only represents the data only read the full Data
+    const snapshot = await userRef.get()
+
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }
+        catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef
+}
+
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
